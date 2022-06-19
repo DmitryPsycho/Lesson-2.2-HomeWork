eabeddd97029c0731787da6e4d6a9f2e36b0c5d6
DmitryPsycho <kovalenko2511@bk.ru>
2022-06-19 16:41:23 +0300

    Traffic light creation

--- Lesson 2.2 HomeWork/ViewController.swift
+++ Lesson 2.2 HomeWork/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var lightSwitchBtn: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+    
+        lightSwitchBtn.setTitle("Switch", for: .normal)
     }
 
+    @IBAction func lightSwitch() {
+            if lightSwitchBtn.currentTitle == "Switch" {
+                lightSwitchBtn.setTitle("Next", for: .normal)
+        }
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowLight.alpha = lightIsOff
+            greenLight.alpha = lightIsOn
+            currentLight = .red
+        }
+    }
+}
 
+extension ViewController {
+    enum CurrentLight {
+        case red, yellow, green
+    }
 }
 
